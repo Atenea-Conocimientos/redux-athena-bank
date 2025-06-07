7420f9f162a82f4959e8fe157928652aef3887be
Juan Torres <juantor16@gmail.com>
2025-06-07 18:44:13 -0300

    Se agrega puerto para vps de NGROK

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       '/api': 'http://localhost:4000',
     },
-    port: 3000,
+    allowedHosts: [
+      'atena-redux.ngrok.app',
+    ],
   },
 })
